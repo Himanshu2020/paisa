cd19ca008fbb43ece00c28678438d83d0fe3755d
Himanshu <techlink.project@gmail.com>
2018-05-25 06:14:11 +0530

    hhhhhhhhhh

--- src/P2p/P2pNetworks.h
+++ src/P2p/P2pNetworks.h
@@ -21,15 +21,8 @@
 #include "../CryptoNoteConfig.h"
 
 #pragma once
-
 namespace CryptoNote
 {
-	namespace
-	{
-		boost::uuids::uuid name;
-		boost::uuids::name_generator gen(name);
-		boost::uuids::uuid u = gen(GENESIS_COINBASE_TX_HEX);
-	}
-	const static boost::uuids::uuid BYTECOIN_NETWORK = u;
-}
+const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0xA1, 0x1A, 0xA1, 0x1A, 0xA1, 0x0A, 0xA1, 0x0A, 0xA0, 0x1A, 0xA0, 0x1A, 0xA0, 0x1A, 0xA1, 0x1A } };
 
+}
